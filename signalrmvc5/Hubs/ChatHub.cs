a90d6758243e90e971ba2ba67f60d4221b48bae3
uporoje <34911292+uporoje@users.noreply.github.com>
2019-11-28 16:14:15 +0300

    Add project files.

---
+++ signalrmvc5/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace signalrmvc5
+{
+    public class ChatHub:Hub
+    {
+        public void SendToAll(string name,string message)
+        {
+            Clients.All.addnewmessage(name, message);
+        }
+        public void SendToOne(string name,string message,string id)
+        {
+            Clients.Client(id).addnewmessage(name, message);
+        }
+    }
+}
